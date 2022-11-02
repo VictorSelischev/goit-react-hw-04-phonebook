160fa93e4b46a0c26e616aaf45bc9a281796f68f
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-02 15:40:11 +0200

    Refactoring component Filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,27 +1,22 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import css from './Filter.module.css';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-class Filter extends Component {
-  render() {
-    const { filter, changeFilter } = this.props;
-    return (
-      <label className={css.formAddBook__label}>
-        Find contacts by name
-        <input
-          className={css.formAddBook__input}
-          type="text"
-          value={filter}
-          onChange={changeFilter}
-        />
-      </label>
-    );
-  }
-}
-
-export { Filter };
+export const Filter = ({ filter, changeFilter }) => {
+  return (
+    <label className={css.formAddBook__label}>
+      Find contacts by name
+      <input
+        className={css.formAddBook__input}
+        type="text"
+        value={filter}
+        onChange={changeFilter}
+      />
+    </label>
+  );
+};
 
 Filter.propTypes = {
   filter: PropTypes.string.isRequired,
   changeFilter: PropTypes.func.isRequired,
-}
+};
